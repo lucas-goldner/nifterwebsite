35cbae6645b4cb4d282d8fe1a8a085c3031190cb
lucas-goldner <lucas.goldner@googlemail.com>
2021-03-27 20:05:07 +0100

    feat: Add meta tags

--- pages/index.js
+++ pages/index.js
@@ -40,6 +40,17 @@ const HomeVerticalSlidePortfolio = () => {
       <Head>
         <title>Nifter Website</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content={
+            "This app brings collecting NFTs to a new level. Show friends all of your valuable bought or traded NFTs from OpenSea or Rarible.We track your current NFTs, if you get a new ones, your friends will be able to see them in their feed.You can discover other users as friends and see their gallery. Maybe their collection could peak your interest.There is no better NFT App on the App Store, so start collecting and download this app!"
+          }
+        />
+        <meta
+          name="keywords"
+          content="Nifter, NFT Nifter, NFT, NFTs,, Opensea, Rarible, Etherium, Trade, Share, Collect, Buy, Mint"
+        />
+        <meta name="author" content="Lucas Goldner" />
       </Head>
 
       <LayoutDefault
